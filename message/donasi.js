fea3ec8451a725b9847259d46acfea5686783a48
*DilGanz* <*qkwjekawokawkk@gmail.com*>
2021-10-31 10:56:44 +0700

    *la*

---
+++ message/donasi.js
@@ -0,0 +1,14 @@
+exports.dodo = (prefix) => {
+return`
+╭────────❲ *DONASI* ❳
+│
+│  Bantu owner kembangkan bot ini
+│  dengan donasi seiklasnya 🙂
+│  Gopay : ga ada pante nanti di blok pula
+│  Shopeepay : 085156137901
+│  dana : 087745612118
+│ Makasih yang udah donasi
+│ dan bantu ngembangin bot ini
+│
+╰────────⏣`
+}
